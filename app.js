2a343f05b97a9a6dc2740ef2345d993f904757e7
Kamal Nayan Kumar <kamal.nkumar@justanswer.com>
2020-08-11 12:03:53 +0530

    removing mongodb integration

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const Express = require("express");
 const Mongoose = require("mongoose");
 const BodyParser = require("body-parser");
 const { response } = require("express");
-const connectDB = require('./connection');
-const Person = require('./Person.model');
+//const connectDB = require('./connection');
+//const Person = require('./Person.model');
 
 const PORT = 8080;
 
@@ -14,12 +14,18 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
 
 
+app.get('/test', (req,res)=>{
+    
+    res.send('node api working sucessfully');
+});
+
 app.post("/person", async (req,res)=>{
 
     try {
+        /*
         var person = new Person(req.body);;
         var result = await person.save();
-        res.send(result);
+        res.send(result);*/
     }
     catch(error){
         response.status(500).send(error);
@@ -31,8 +37,10 @@ app.post("/person", async (req,res)=>{
 app.get("/people", async (req, res)=>{
 
     try {
-    var result = await Person.find().exec();
-    res.send(result);
+        /*
+        var result = await Person.find().exec();
+        res.send(result);*/
+        res.send('node api working...');
     }
     catch(err){
         res.status(500).send(err);
@@ -43,8 +51,10 @@ app.get("/people", async (req, res)=>{
 app.get("/person/:id", async (req,res) =>{
 
     try {
+        /*
     var person = await Person.findById(req.params.id).exec();
-    res.send(person);
+    res.send(person);*/
+    res.send('node api working...');
     }
     catch(err){
         res.status(500).send(err);
@@ -54,11 +64,12 @@ app.get("/person/:id", async (req,res) =>{
 app.put("/person/:id", async (req, res)=>{
 
     try {
-
+        /*
         var person = await Person.findById(req.params.id).exec();        
         person.set(req.body);
         var result = await person.save();
-        res.send(result);
+        res.send(result);*/
+        res.send('node api working...');
     }    
     catch(err){
         res.status(500).send(error);
@@ -67,8 +78,10 @@ app.put("/person/:id", async (req, res)=>{
 
 app.delete("/person/:id", async(req,res) => {
     try {
+        /*
         var result = await Person.deleteOne({_id: req.params.id}).exec();
-        res.send(result);        
+        res.send(result); */       
+        res.send('node api working...');
     }
     catch(err){
         response.status(500).send(err);
@@ -79,7 +92,8 @@ app.listen(PORT, () => {
 
     console.log(`server listening at port ${PORT} `);
 
+    /*
     connectDB().then(()=>{
         console.log("mongodb connected");
-    });
+    }); */
 });
